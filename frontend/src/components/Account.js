65c59e374c7d7cf0e6135fa8717fa53b1d68bdd8
ztandrews <68918006+ztandrews@users.noreply.github.com>
2022-04-22 08:04:45 -0400

    Added update free time

--- frontend/src/components/Account.js
+++ frontend/src/components/Account.js
@@ -5,7 +5,10 @@ import NavbarComp from './NavbarComp'
 export default class Account extends Component {
 
     state = {
-        userData: []
+        userData: [],
+        newFreeTime: '',
+        alertMessage: '',
+        fontColor: ''
     }
 
     componentDidMount(){
@@ -14,6 +17,31 @@ export default class Account extends Component {
             const currentUser = res.data.data[0];
             this.setState({userData: currentUser});
         })
+    }
+
+    handleChange = (e) => {
+        this.setState({[e.target.name] : e.target.value})
+        console.log(e.target.value);
+    }
+
+    handleSubmit = event => {
+        console.log(this.state.newFreeTime)
+        const user = localStorage.getItem('user_id')
+        if (this.state.newFreeTime === ''){
+            this.setState({fontColor: '#dc3545'})
+            this.setState({alertMessage: "Failed to update free time. Please fill out the field."})
+        }else{
+            axios.put(`http://localhost:8000/users/${user}/${this.state.newFreeTime}`).then(res =>{
+                if (res.data.status == "ok"){
+                    this.setState({fontColor:'#5aa864'})
+                    this.setState({alertMessage: "Free time successfully updated!"});
+                }
+                else{
+                    this.setState({fontColor:'#dc3545'})
+                    this.setState({alertMessage: "Failed to update free time."});
+                }
+            })
+        }
     }
 
   render() {
@@ -28,12 +56,13 @@ export default class Account extends Component {
                         <h4>Current free time: {this.state.userData.free_time} Hours</h4>
                         <br></br>
                         <h4>Update free time</h4>
-                        <input placeholder='New free time'></input>
+                        <input name="newFreeTime" placeholder='New free time' value={this.state.newFreeTime} onChange={this.handleChange}></input>
                         <br></br>
                         <br></br>
-                        <button className='btn btn-primary'>Submit</button>
+                        <button className='btn btn-primary' onClick={() => this.handleSubmit()}>Submit</button>
                         <br></br>
                         <br></br>
+                        <h4 style={{color: this.state.fontColor}}>{this.state.alertMessage}</h4>
                     </center>
                 </div>
             </div>
